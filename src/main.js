6c8c382c0f60919a99cc86b80fe1a5fffe014bac
blueknightxzx-jpg <blueknightxzx@gmail.com>
2026-09-20 19:22:47 +0530

    v0.4: cache-bust module chain

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import { CONFIG } from "./core/Config.js?v=v0.3.2";
-import { Input } from "./core/Input.js?v=v0.3.2";
-import { Renderer } from "./core/Renderer.js?v=v0.3.2";
-import { StateMachine } from "./core/StateMachine.js?v=v0.3.2";
-import { Game } from "./core/Game.js?v=v0.3.2";
-import { Camera } from "./core/Camera.js?v=v0.3.2";
-import { BootScene } from "./scenes/BootScene.js?v=v0.3.2";
-import { MenuScene } from "./scenes/MenuScene.js?v=v0.3.2";
-import { CastleScene } from "./scenes/CastleScene.js?v=v0.3.2";
+import { CONFIG } from "./core/Config.js?v=v0.4.0";
+import { Input } from "./core/Input.js?v=v0.4.0";
+import { Renderer } from "./core/Renderer.js?v=v0.4.0";
+import { StateMachine } from "./core/StateMachine.js?v=v0.4.0";
+import { Game } from "./core/Game.js?v=v0.4.0";
+import { Camera } from "./core/Camera.js?v=v0.4.0";
+import { BootScene } from "./scenes/BootScene.js?v=v0.4.0";
+import { MenuScene } from "./scenes/MenuScene.js?v=v0.4.0";
+import { CastleScene } from "./scenes/CastleScene.js?v=v0.4.0";
 
 function showRuntimeError(error) {
   const message = error instanceof Error ? error.message : String(error);
